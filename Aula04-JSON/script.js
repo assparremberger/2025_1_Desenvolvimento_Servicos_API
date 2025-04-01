518fd48c6be1b12aa7a366ef7d82b8dbedd84752
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2025-04-01 12:05:29 -0300

    Aula04 JSON

--- Aula04-JSON/script.js
+++ Aula04-JSON/script.js
@@ -32,3 +32,36 @@ function lerJSON(){
     req.open("GET", "dados.json", true);
     req.send()
 }
+
+function consultar(){
+    var req = new XMLHttpRequest();
+
+    req.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200 ){
+            var objJSON = JSON.parse( this.responseText );
+            if( objJSON.resposta ){
+                alert( objJSON.resposta );
+            }else{
+                txt =   '<table border="1" >';
+                txt +=  '   <tr>';
+                txt +=  '       <th>Id</th>';
+                txt +=  '       <th>Nome</th>';
+                txt +=  '       <th>Preço</th>';
+                txt +=  '   </tr>';
+                objJSON.produtos.forEach( prod => {
+                    txt += '<tr>';
+                    txt += '    <td>' + prod.id + '</td>';
+                    txt += '    <td>' + prod.nome + '</td>';
+                    txt += '    <td>' + prod.preco + '</td>';
+                    txt += '</tr>';
+                });
+                txt += '</table>';
+                document.getElementById("divProdutos").innerHTML = txt;
+            }
+        }else if( this.readyState == 4 ){
+            alert( this.status + " - " + this.statusText );
+        }
+    };
+    req.open("GET", "servidor.php?consultar", true);
+    req.send()
+}
